2cb6b56cc79e03ebe65bfd17d467f21818ed9a23
Saptarshi Laha <saptarshi.laha@gmail.com>
2022-10-19 00:12:03 +0100

    Fixed Commit.

---
+++ Shared/connectionStrings.php
@@ -0,0 +1,13 @@
+<?php
+$mysql_host = 'localhost';
+$mysql_db = "AutoRemediateDB";
+
+$mysql_username_root = "root";
+$mysql_password_root = "root";
+$mysql_connectionString_root = "mysql:host=$mysql_host;charset=UTF8";
+
+$mysql_username_user = "user";
+$mysql_password_user = "password";
+$mysql_connectionString_user_AutoRemediateDB = "mysql:host=$mysql_host;dbname=$mysql_db;charset=UTF8";
+
+
